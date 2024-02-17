f314b6767e9b3fb8ec054dbe7383c82e2a77c8eb
Greatcthulhu13 <144727889+Greatcthulhu13@users.noreply.github.com>
2024-02-17 10:36:51 +0000

    Update debiansetup.sh

--- Setup Scripts/debiansetup.sh
+++ Setup Scripts/debiansetup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Update package lists and upgrade existing packages\
+# Update package lists and upgrade existing packages
 
 sudo apt update && sudo apt upgrade -y
 
@@ -10,13 +10,14 @@ sudo apt config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/39/w
 
 sudo apt install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 
-sudo apt install -y  neofetch lolcat winehq-stable ani-cli firefox thunderbird konsole kate audacity speedtest-cli grep sed curl mpv aria2 yt-dlp ffmpeg fzf patch ani-cli timeshift steam lutris vlc kdenlive qbittorrent nano libreoffice\
+sudo apt install -y  neofetch lolcat winehq-stable ani-cli firefox thunderbird konsole kate audacity speedtest-cli grep sed curl mpv aria2 yt-dlp ffmpeg fzf patch ani-cli timeshift steam lutris vlc kdenlive qbittorrent nano libreoffice
 
 
-# Additional configuration steps\
-git clone "https://github.com/pystardust/ani-cli.git"\
-sudo cp ani-cli/ani-cli /usr/local/bin\
-rm -rf ani-cli\
-\
-# Print completion message\
-echo "Setup complete!"}
+# Additional configuration steps
+
+git clone "https://github.com/pystardust/ani-cli.git"
+sudo cp ani-cli/ani-cli /usr/local/bin
+rm -rf ani-cli
+
+# Print completion message
+echo "Setup complete!"
