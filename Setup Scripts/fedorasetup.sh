8f7ddc65e09857c6e99ad4e6e5a19a3604f17624
Greatcthulhu13 <144727889+Greatcthulhu13@users.noreply.github.com>
2024-02-17 10:40:28 +0000

    Update fedorasetup.sh

--- Setup Scripts/fedorasetup.sh
+++ Setup Scripts/fedorasetup.sh
@@ -9,6 +9,8 @@ sudo dnf copr enable derisis13/ani-cli
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install -y  neofetch lolcat winehq-stable ani-cli firefox thunderbird konsole kate audacity speedtest-cli grep sed curl mpv aria2 yt-dlp ffmpeg fzf patch ani-cli timeshift steam lutris vlc kdenlive qbittorrent nano libreoffice
 
+# Configure preferences 
+echo "alias ll='ls -alF'" >> ~/.bashrc
 
 # Additional configuration steps
 git clone "https://github.com/pystardust/ani-cli.git"
