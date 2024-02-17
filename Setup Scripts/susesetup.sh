547eb4f93b00fe88030d80d7ce609ede7fa90e09
Greatcthulhu13 <144727889+Greatcthulhu13@users.noreply.github.com>
2024-02-17 10:59:45 +0000

    Create susesetup.sh

---
+++ Setup Scripts/susesetup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Refresh Zypper repository metadata
+sudo zypper refresh
+
+# Update installed packages
+sudo zypper update -y 
+
+# Install favorite programs from official repositories
+sudo zypper install -y git neofetch lolcat firefox thunderbird konsole kate audacity speedtest-cli grep sed curl mpv aria2 yt-dlp ffmpeg fzf patch ani-cli timeshift steam lutris vlc kdenlive qbittorrent nano libreoffice
+
+zypper addrepo https://download.copr.fedorainfracloud.org/results/derisis13/ani-cli/opensuse-tumbleweed-x86_64/ ani-cli
+zypper dup
+zypper install ani-cli
+
+# Add additional configuration steps as needed
+git clone "https://github.com/pystardust/ani-cli.git"
+sudo cp ani-cli/ani-cli /usr/local/bin
+rm -rf ani-cli
+
+# Print completion message
+echo "Setup complete!"
