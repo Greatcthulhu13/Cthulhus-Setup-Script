457c30006bcc34abf01add9036a35b4acc4190e2
Greatcthulhu13 <144727889+Greatcthulhu13@users.noreply.github.com>
2024-02-17 10:49:29 +0000

    Update fedorasetup.sh

--- Setup Scripts/fedorasetup.sh
+++ Setup Scripts/fedorasetup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # Update package lists and upgrade existing packages
-
 sudo dnf update && sudo dnf upgrade -y
 
 # Install favorite programs
-
 sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/39/winehq.repo
 
 sudo dnf copr enable derisis13/ani-cli
@@ -17,8 +15,7 @@ sudo dnf install -y git neofetch lolcat winehq-stable ani-cli firefox thunderbir
 # Configure preferences 
 echo "alias ll='ls -alF'" >> ~/.bashrc
 
-# Additional configuration steps
-
+# Additional configuration
 git clone "https://github.com/pystardust/ani-cli.git"
 sudo cp ani-cli/ani-cli /usr/local/bin
 rm -rf ani-cli
