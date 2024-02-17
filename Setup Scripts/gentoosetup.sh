8ed3979ab07bb9326ffa17b5fc5c2a91768d9690
Greatcthulhu13 <144727889+Greatcthulhu13@users.noreply.github.com>
2024-02-17 10:54:57 +0000

    Create gentoosetup.sh

---
+++ Setup Scripts/gentoosetup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update Portage tree
+sudo emerge --sync
+
+# Update installed packages
+sudo emerge -avuDN @world
+
+# Install favorite programs from Portage
+sudo emerge -av git neofetch lolcat winehq-stable ani-cli firefox thunderbird konsole kate audacity speedtest-cli grep sed curl mpv aria2 yt-dlp ffmpeg fzf patch ani-cli timeshift steam lutris vlc kdenlive qbittorrent nano libreoffice
+
+sudo eselect repository enable guru
+sudo emaint sync -r guru
+sudo emerge -a ani-cli
+sudo emerge -a =app-misc/ani-cli-9999
+
+# Additional configuration 
+git clone "https://github.com/pystardust/ani-cli.git"
+sudo cp ani-cli/ani-cli /usr/local/bin
+rm -rf ani-cli
+
+# Print completion message
+echo "Setup complete!"
