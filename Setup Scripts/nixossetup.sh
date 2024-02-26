6a51d1c392ceb6546ab94700db87bb66a2675dab
Cthulhu <144727889+Greatcthulhu13@users.noreply.github.com>
2024-02-26 14:22:33 +0000

    Update nixossetup.sh

--- Setup Scripts/nixossetup.sh
+++ Setup Scripts/nixossetup.sh
@@ -9,6 +9,8 @@ sudo nix-env -u
 # Install favorite programs from Nixpkgs
 sudo nix-env -iA git fastfetch lolcat firefox thunderbird konsole kate audacity speedtest-cli grep sed curl mpv aria2c yt-dlp ffmpeg fzf patch ani-skip timeshift steam lutris vlc kdenlive qbittorrent nano libreoffice mu
 
+nix-shell -p fastfetch
+
 # Add additional configuration steps as needed
 git clone "https://github.com/pystardust/ani-cli.git"
 sudo cp ani-cli/ani-cli /usr/local/bin
